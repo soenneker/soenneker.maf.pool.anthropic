28177d165c1002c9eab957d922f29eda1f6d26e3
Jake Soenneker <jake@soenneker.com>
2026-04-21 19:36:05 -0500

    cleanup

--- test/Soenneker.Maf.Pool.Anthropic.Tests/MafPoolAnthropicExtensionTests.cs
+++ test/Soenneker.Maf.Pool.Anthropic.Tests/MafPoolAnthropicExtensionTests.cs
@@ -3,13 +3,8 @@ namespace Soenneker.Maf.Pool.Anthropic.Tests;
 
 public sealed class MafPoolAnthropicExtensionTests
 {
-    public MafPoolAnthropicExtensionTests( output)
-    {
-    }
-
     [Test]
     public void Default()
     {
-
     }
 }
